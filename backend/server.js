3d69126d5c08dfab19a511143aab72f34f7784ef
Kevin <kevininfant.pa.2024.it@rajalakshmi.edu.in>
2026-04-28 15:30:17 +0530

    test route

--- backend/server.js
+++ backend/server.js
@@ -69,3 +69,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/test-budget", (req, res) => {
+  res.send("Budget route working");
+});
